c136e6fd4ee69b5d09c505ec202c2b3feb6a61fe
raphaelrpl <raphael.wcosta@gmail.com>
2020-03-17 08:46:51 -0300

    Improvelicense header description

--- cube_builder/celery/worker.py
+++ cube_builder/celery/worker.py
@@ -2,17 +2,11 @@
 # This file is part of Python Module for Cube Builder.
 # Copyright (C) 2019-2020 INPE.
 #
-# Cube Builder free software; you can redistribute it and/or modify it
+# Cube Builder is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 #
 
 """Define a structure component to run celery worker."""
-
-# Python Native
-import logging
-
-# 3rdparty
-from celery.signals import worker_shutdown
 
 # Builder
 from cube_builder import create_app
